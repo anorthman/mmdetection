cebac3987cac4c66fa859d64c9b3500a9d2a3beb
anorthman <940769395@qq.com>
2019-07-05 18:01:04 +0800

    update baseconv and config

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -2,4 +2,5 @@ from .resnet import ResNet
 from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
 from .shufv2 import Shufv2
-__all__ = ['ResNet', 'ResNeXt', 'SSDVGG', 'Shufv2']
+from .base import Base
+__all__ = ['ResNet', 'ResNeXt', 'SSDVGG', 'Shufv2','Base']
