d825452c7b546d94ec1f0afd7b864a5a08ba8f69
you <you@example.com>
2019-08-06 06:04:23 -0400

    shuf2_nie

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -3,4 +3,5 @@ from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
 from .shufv2 import Shufv2
 from .base import Base
-__all__ = ['ResNet', 'ResNeXt', 'SSDVGG', 'Shufv2','Base']
+from .shufv2_nie import Shufv2_nie
+__all__ = ['ResNet', 'ResNeXt', 'SSDVGG', 'Shufv2','Base', 'Shufv2_nie']
